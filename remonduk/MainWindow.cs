2ebdaec6b15861fe90d9c09ba89dadc7ae7783b6
JRemondi <JRemondi@gmail.com>
2014-12-13 21:34:03 -0500

    Stuff!

---
+++ remonduk/MainWindow.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace remonduk
+{
+    public partial class MainWindow : Form
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Application.Idle += HandleApplicationIdle;  //adds the HandleApplicationIdle method to the Application.Idle event
+
+        }
+
+        //this method is added to the Idle event in the constructor
+        void HandleApplicationIdle(object sender, EventArgs e)
+        {
+            while (IsApplicationIdle()) //keep going while we're still idle. idle event is fired once when the queue is emptied
+            {
+
+            }
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct NativeMessage
+        {
+            public IntPtr Handle;
+            public uint Message;
+            public IntPtr WParameter;
+            public IntPtr LParameter;
+            public uint Time;
+            public Point Location;
+        }
+
+        //check to see the next message in queue returns 0 if empty 
+        [DllImport("user32.dll")]
+        public static extern int PeekMessage(out NativeMessage message, IntPtr window, uint filterMin, uint filterMax, uint remove);
+
+        //check to see if there's anything in the queue
+        bool IsApplicationIdle()
+        {
+            NativeMessage result;
+            return PeekMessage(out result, IntPtr.Zero, (uint)0, (uint)0, (uint)0) == 0;
+        }
+    }
+}
