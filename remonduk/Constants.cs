1b44fe42ee2e69aaa0b95748d7834e1a1973248d
jkuk <jkuk@umass.edu>
2014-12-16 22:01:32 -0500

    my stuff wants to merge with your stuff

---
+++ remonduk/Constants.cs
@@ -0,0 +1,5 @@
+namespace remonduk {
+	public static class Constants {
+		public const float GRAVITY = 9.8F;
+	}
+}
---
+++ remonduk/Physics.cs
@@ -0,0 +1,6 @@
+namespace constants
+{
+	public class constants {
+		public const float GRAVITY = 9.8F;
+	}
+}
