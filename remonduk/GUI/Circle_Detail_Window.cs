6f749100071564e33ca49e98d36dbee23d224d02
JRemondi <JRemondi@gmail.com>
2014-12-20 12:00:43 -0500

    New GUI objects

---
+++ remonduk/GUI/Circle_Detail_Window.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace remonduk.GUI
+{
+    public partial class Circle_Detail_Window : Form
+    {
+        Circle c;
+
+        public Circle_Detail_Window()
+        {
+            InitializeComponent();
+        }
+
+        public Circle_Detail_Window(Circle c, List<Interaction> interactions)
+        {
+            InitializeComponent();
+            this.c = c;
+            color_button.BackColor = c.color;
+            update_circle(c, interactions);
+            this.Paint += new PaintEventHandler(this.drawNewCircleAngles);
+            
+        }
+
+        void drawNewCircleAngles(object sender, System.Windows.Forms.PaintEventArgs e)
+        {
+            Graphics g = this.CreateGraphics();
+            Pen pen = new Pen(Color.Black);
+            double theta = (double)velocity_angle_up_down.Value;
+            float x1 = 52.5F;
+            float y1 = 127.5F;
+            float x2 = (float)Math.Cos(theta * (Math.PI / 180.0)) * 25 + x1;
+            float y2 = (float)Math.Sin(theta * (Math.PI / 180.0)) * 25 + y1;
+            g.DrawLine(pen, x1, y1, x2, y2);
+            g.DrawEllipse(pen, 15, 90, 75, 75);
+
+
+            theta = (double)acceleration_angle_up_down.Value;
+            //System.Diagnostics.Debug.WriteLine(new_circle_acceleration_angle_up_down.Value);
+            x1 = 162.5F;
+            y1 = 127.5F;
+            x2 = (float)Math.Cos(theta * (Math.PI / 180.0)) * 25 + x1;
+            y2 = (float)Math.Sin(theta * (Math.PI / 180.0)) * 25 + y1;
+            g.DrawLine(pen, x1, y1, x2, y2);
+            g.DrawEllipse(pen, 125, 90, 75, 75);
+        }
+
+        public void update_circle(Circle c, List<Interaction> interactions)
+        {
+            acceleration_up_down.Value = (Decimal)c.acceleration;
+            velocity_up_down.Value = (Decimal)c.velocity;
+            acceleration_angle_up_down.Value = (Decimal)(c.acceleration_angle * 180.0/Math.PI);
+            velocity_angle_up_down.Value = (Decimal)(c.velocity_angle * 180.0/Math.PI);
+            List<Interaction> real = new List<Interaction>();
+            foreach(Interaction interaction in interactions)
+            {
+                if(interaction.first == c || interaction.second == c)
+                {
+                    real.Add(interaction);
+                }
+            }
+            this.interactions_list.DataSource = real;
+            this.interactions_list.DisplayMember = "Force";
+            color_button.BackColor = c.color;
+            this.c = c;
+            this.Invalidate();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void velocity_label_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void color_button_Click(object sender, EventArgs e)
+        {
+            circle_color_dialog.ShowDialog();
+            c.color = circle_color_dialog.Color;
+            color_button.BackColor = circle_color_dialog.Color;
+        }
+    }
+}
---
+++ remonduk/GUI/Physical_System_Detail_Window.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace remonduk.GUI
+{
+    public partial class Physical_System_Detail_Window : Form
+    {
+        PhysicalSystem ps;
+        public Interaction selected_interaction;
+        public Circle selected_circle;
+        int last_interaction_count;
+        int last_circle_count;
+
+        public Physical_System_Detail_Window()
+        {
+            InitializeComponent();
+        }
+
+        public Physical_System_Detail_Window(PhysicalSystem ps)
+        {
+            InitializeComponent();
+            this.ps = ps;
+            selected_interaction = null;
+            selected_circle = null;
+            update_ps(ps);
+        }
+
+        public void update_ps(PhysicalSystem ps)
+        {
+            if(last_interaction_count != ps.interactions.Count)
+            {
+                interactions_list.DataSource = null;
+                interactions_list.DataSource = ps.interactions;
+                this.Invalidate();
+                last_interaction_count = ps.interactions.Count;
+            }
+            if(last_circle_count != ps.netForces.Keys.Count)
+            {
+                circle_list.DataSource = null;
+                circle_list.DataSource = ps.netForces.Keys.ToArray();
+                this.Invalidate();
+                last_circle_count = ps.netForces.Keys.Count;
+            }
+            if (interactions_list.SelectedItem != null)
+            {
+                selected_interaction = (Interaction)interactions_list.SelectedItem;
+            }
+            if(circle_list.SelectedItem != null)
+            {
+                selected_circle = (Circle)circle_list.SelectedItem;
+            }
+        }
+
+        private void interactions_remove_button_Click(object sender, EventArgs e)
+        {
+            ps.removeInteraction(selected_interaction);
+        }
+
+        private void circle_list_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+
+            
+        }
+
+        private void circle_list_MouseClick(object sender, MouseEventArgs e)
+        {
+            selected_circle = (Circle)circle_list.SelectedItem;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ps.removeCircle(selected_circle);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (selected_circle != null)
+            {
+                GUI.Circle_Detail_Window cdw = GUI_Singleton.Instance.cdw;
+                cdw.update_circle(selected_circle, ps.interactions);
+                cdw.Visible = true;
+                cdw.Focus();
+            }
+        }
+    }
+}
