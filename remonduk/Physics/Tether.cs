f341d7e4efa97ec83bef96b907a7a2939d16baf8
jkuk <j_kuk@post.massbay.edu>
2015-01-16 08:34:15 -0500

    implemented a quick damping force on tethers

--- remonduk/Physics/Tether.cs
+++ remonduk/Physics/Tether.cs
@@ -20,24 +20,29 @@ namespace Remonduk.Physics
 				delegate(Circle first, Circle second)
 				{
 					equilibrium = first.Radius + second.Radius;
-					double dist = first.Distance(second);
-					if (dist > equilibrium)
-					{
-						dist -= equilibrium;
-						double f = k * dist;
 
-						double delta_x = second.Px - first.Px;
-						double delta_y = second.Py - first.Py;
-						double angle = OrderedPair.Angle(delta_y, delta_x);
+					double fx = k * (second.Px - first.Px) + .01 * (second.Vx - first.Vx);
+					double fy = k * (second.Py - first.Py) + .01 * (second.Vy - first.Vy);
 
-						double fx = f * Math.Cos(angle);
-						double fy = f * Math.Sin(angle);
-						return new OrderedPair(fx, fy);
-					}
-					else
-					{
-						return new OrderedPair(0.0, 0.0);
-					}
+					return new OrderedPair(fx, fy);
+					//double dist = first.Distance(second);
+					//if (dist > equilibrium)
+					//{
+					//	dist -= equilibrium;
+					//	double f = k * dist;
+
+					//	double delta_x = second.Px - first.Px;
+					//	double delta_y = second.Py - first.Py;
+					//	double angle = OrderedPair.Angle(delta_y, delta_x);
+
+					//	double fx = f * Math.Cos(angle);
+					//	double fy = f * Math.Sin(angle);
+					//	return new OrderedPair(fx, fy);
+					//}
+					//else
+					//{
+					//	return new OrderedPair(0.0, 0.0);
+					//}
 				}
 			)
 		{ }
